eee5e084dd6fd2c4a606d11c5adae91eebfec895
Дмитрий Плеханов <Tyuiu.PlehanovDS@outlook.com>
2026-09-10 02:22:12 +0500

    Task2

---
+++ Tyuiu.PlehanovDS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PlehanovDS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PlehanovDS.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Дмитрий"));
+            Console.ReadKey();
+        }
+    }
+}
